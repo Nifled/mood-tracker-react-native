8c93c16c382e1279333aeea8938a56a16310f8c6
Erick Delfin <erickdelfinmolina@gmail.com>
2023-07-12 15:07:16 -0700

    Style the mood picker

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Mood } from '../types';
 import { PressableArea } from './PressableArea';
+import { theme } from '../theme';
 
 const MOODS: Mood[] = [
   { emoji: '🧑‍💻', description: 'studious' },
@@ -15,26 +16,34 @@ export const MoodPicker: React.FC = () => {
   const [selectedMood, setSelectedMood] = React.useState<Mood>();
 
   return (
-    <View style={styles.moodList}>
-      {MOODS.map(mood => (
-        <View>
-          <PressableArea
-            onPress={() => setSelectedMood(mood)}
-            key={mood.emoji}
-            style={[
-              styles.moodItem,
-              mood.emoji === selectedMood?.emoji
-                ? styles.selectedMoodItem
-                : undefined,
-            ]}>
-            <Text style={styles.moodText}>{mood.emoji}</Text>
-          </PressableArea>
+    <View style={styles.container}>
+      <Text style={styles.heading}>How are you right now?</Text>
 
-          <Text style={styles.descriptionText}>
-            {selectedMood?.emoji === mood.emoji ? mood.description : ' '}
-          </Text>
-        </View>
-      ))}
+      <View style={styles.moodList}>
+        {MOODS.map(mood => (
+          <View>
+            <PressableArea
+              onPress={() => setSelectedMood(mood)}
+              key={mood.emoji}
+              style={[
+                styles.moodItem,
+                mood.emoji === selectedMood?.emoji
+                  ? styles.selectedMoodItem
+                  : undefined,
+              ]}>
+              <Text style={styles.moodText}>{mood.emoji}</Text>
+            </PressableArea>
+
+            <Text style={styles.descriptionText}>
+              {selectedMood?.emoji === mood.emoji ? mood.description : ' '}
+            </Text>
+          </View>
+        ))}
+      </View>
+
+      <PressableArea style={styles.button}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </PressableArea>
     </View>
   );
 };
@@ -58,13 +67,40 @@ const styles = StyleSheet.create({
   },
   selectedMoodItem: {
     borderWidth: 2,
-    backgroundColor: '#454C73',
-    borderColor: '#fff',
+    backgroundColor: theme.colors.purple,
+    borderColor: theme.colors.white,
   },
   descriptionText: {
-    color: '#454C73',
+    color: theme.colors.purple,
     fontWeight: 'bold',
     fontSize: 10,
     textAlign: 'center',
+  },
+  container: {
+    borderWidth: 2,
+    borderColor: theme.colors.purple,
+    margin: 10,
+    borderRadius: 10,
+    padding: 20,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: theme.colors.purple,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  buttonText: {
+    color: theme.colors.white,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
---
+++ src/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  colors: {
+    purple: '#454C73',
+    white: '#fff',
+  },
+};
